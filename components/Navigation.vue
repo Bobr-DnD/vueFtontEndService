ad05d91e8d11452854643597e035331a1d5395e9
cyberpOOP <vlad0021135@gmail.com>
2025-08-07 23:23:22 +0300

    init commit

---
+++ components/Navigation.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/sessions">Сесії</RouterLink>
+    </nav>
+</template>
+
+<style scope></style>
---
+++ views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import Navigation from '@components/Navigation.vue';
+</script>
+
+<template>
+    <Navigation />
+    <h1>Home</h1>
+</template>
+
+<style scoped></style>
