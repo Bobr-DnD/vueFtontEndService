7468d6731a0e2a5b4327f304650624c5952b2fdd
CyberpOOP <vlad0021135@gmail.com>
2025-10-30 18:47:41 +0200

    custom fields add button init

--- src/components/reusable/CustomFieldsTable.vue
+++ src/components/reusable/CustomFieldsTable.vue
@@ -4,6 +4,7 @@ import HideButton from '../reusable/HideButton.vue';
 import FormAddSubtract from '@/components/reusable/FormAddSubtract.vue';
 import FormString from '@/components/reusable/FormString.vue';
 import { ChartBarIcon } from '@heroicons/vue/24/solid';
+import PlusButton from './PlusButton.vue';
 
 const props = defineProps({
     fields: { type: Object, required: true },
@@ -11,11 +12,18 @@ const props = defineProps({
 })
 
 let custom_hidden = ref(true)
+let modal_hidden = ref(true)
 
 function updateFields(name, value){
     if(typeof(value) === 'string') props.fields[name] = value
     else props.fields[name] += value
     props.callback(props.fields)
+}
+
+function showModal(){
+    modal_hidden.value = !modal_hidden.value
+    console.log(modal_hidden.value);
+    
 }
 </script>
 
@@ -29,5 +37,17 @@ function updateFields(name, value){
         <FormAddSubtract v-if="typeof (value) === 'number'" :label="'CustomFields_' + name" :entity_name="name" :value="props.fields[name]"
         :callback="updateFields" class="mx-auto shadow-[rgba(0,0,0,0.5)_0px_4px_16px]"/>
 
+        <PlusButton @click="showModal" class="w-16 mx-auto text-center border-4 border-darkred-dark rounded-lg 
+           transition-all duration-300 ease-out
+           hover:scale-110 hover:border-darkred-red hover:shadow-[0_0_15px_#8E1616] hover:cursor-pointer
+           bg-gradient-to-br from-darkred-dark to-darkred-light
+           hover:from-darkred-red hover:to-darkred-dark relative overflow-hidden group"/>
+
     </div>
+
+    <div>
+        <form action=""></form>
+    </div>
+
+
 </template>
---
+++ src/components/reusable/PlusButton.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { PlusIcon } from '@heroicons/vue/24/solid';
+</script>
+
+<template>
+    <div><PlusIcon class="w-14 h-14 text-center rounded-lg transition-transform duration-300
+             group-hover:rotate-90 group-hover:text-darkred-light"/></div>
+</template>
