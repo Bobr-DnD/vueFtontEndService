07d74f1bd86dfc1da3e76a7665061574c4d5ccfe
cyberpOOP <vlad0021135@gmail.com>
2025-11-16 20:39:02 +0200

    server port from env

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,27 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    //vueDevTools(),
-  ],
-  server:{
-    port: 8080,
-    host: '0.0.0.0',
-    allowedHosts: ['fallout-dnd.tplinkdns.com']
-  },
-  resolve: {
-    alias: {
-      '@http': fileURLToPath(new URL('./HTTPServices', import.meta.url)),
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+      vue(),
+      //vueDevTools(),
+    ],
+    server: {
+      port: env.VITE_PORT,
+      host: '0.0.0.0',
+      allowedHosts: ['fallout-dnd.tplinkdns.com']
     },
-  },
+    resolve: {
+      alias: {
+        '@http': fileURLToPath(new URL('./HTTPServices', import.meta.url)),
+        "@ws": fileURLToPath(new URL('./WSServices', import.meta.url)),
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
 })
