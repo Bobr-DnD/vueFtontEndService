46fbe25d8d3298fb194c51c3039849ef9396467c
CyberpOOP <vlad0021135@gmail.com>
2025-11-28 17:53:58 +0200

    selector refactor styles

--- src/components/reusable/SingleFieldValueSelector.vue
+++ src/components/reusable/SingleFieldValueSelector.vue
@@ -29,12 +29,34 @@ const props = defineProps({
     <div class="flex flex-col items-start justify-center gap-2">
         <div class="text-lg font-gothic">{{ props.label }}:</div>
 
-        <select class="w-full flex items-center gap-4 p-2 rounded-xl bg-darkred-dark text-darkred-light"
-            :name="props.label" :id="props.field_name" @change="props.callback(props.field_name, $event.target.value)">
-            <option class="p-1" v-for="option in props.options" :value="option" :selected="props.status ? true : false">
-                {{ option }}</option>
-        </select>
+        <div class="relative w-full">
+            
+            <select class="w-full p-3 pr-10 rounded-xl bg-darkred-dark text-darkred-light font-gothic
+           borde border-darkred-red/40 shadow-md outline-none cursor-pointer
+           transition-all duration-200 focus:border-darkred-red focus:ring-2 focus:ring-darkred-red/40
+           hover:bg-darkred-dark/80" :name="props.label" :id="props.field_name"
+                @change="props.callback(props.field_name, $event.target.value)">
+
+                <option class="bg-darkred-dark text-darkred-light hover:bg-darkred-red/20 p-2"
+                    v-for="option in props.options" :value="option" :selected="props.status ? true : false">
+                    {{ option }}
+                </option>
+
+            </select>
+
+            <div class="pointer-events-none absolute right-4 top-1/2 -translate-y-1/2
+           text-darkred-light transition-all duration-200
+           group-hover:rotate-180">
+                ▼
+            </div>
+        </div>
 
     </div>
 
 </template>
+
+<style scoped>
+select {
+    appearance: none;
+}
+</style>
